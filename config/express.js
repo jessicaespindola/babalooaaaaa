39171d47d1e5bce58d27a7682264c40122a32ed5
Jéssica Espíndola da Silva <39319821+jessicaespindola@users.noreply.github.com>
2020-11-11 11:10:21 -0300

    Adicionada as Configs

---
+++ config/express.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+/*
+consign()
+  .include('models')
+  .then('resources')
+  .into(app);
+*/
+module.exports = app;
